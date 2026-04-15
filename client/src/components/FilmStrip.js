1d5d7c1c771770ec3d700a4ccd110381a01e263d
nehaadixitt <dixitneha564@gmail.com>
2026-04-15 20:55:49 +0530

    Use locally hosted movie posters in FilmStrip

--- client/src/components/FilmStrip.js
+++ client/src/components/FilmStrip.js
@@ -2,20 +2,17 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const FILM_STILLS = [
-  'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg', // Inception
-  'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg', // Interstellar
-  'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg', // Joker
-  'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NiYyLTg3MzMtYTJmNjg3Nzk5MzRiXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_SX300.jpg', // Shawshank
-  'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg', // Dark Knight
-  'https://m.media-amazon.com/images/M/MV5BNTIwMDYxNTc5NF5BMl5BanBnXkFtZTcwNjY2NjU3MQ@@._V1_SX300.jpg', // 3 Idiots
-  'https://m.media-amazon.com/images/M/MV5BNzIwMTYxMjItZGZlMy00YzJiLTgxZWEtZmI4ZWNmMWJlZTZiXkEyXkFqcGdeQXVyNjQ2MjQ5NM@@._V1_SX300.jpg', // Dangal
-  'https://m.media-amazon.com/images/M/MV5BZTNlNjZmNjMtYzU2ZS00ZDkxLWI1NDMtZDg5ZjkzMGM3ZTVmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg', // DDLJ
-  'https://m.media-amazon.com/images/M/MV5BODQ1MjYyMDMtNDY2MC00ZjE5LWFhNTMtNDQ2YjgxNGIzMGY4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg', // Sholay
-  'https://m.media-amazon.com/images/M/MV5BZjI2ZDI5YzQtNTdlZS00NWQ4LWI1ZjEtMmJlZjU3Zjk5MzE3XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg', // Gully Boy
-  'https://m.media-amazon.com/images/M/MV5BMjA5NTE4NTE5NV5BMl5BanBnXkFtZTYwNzY3MDg2._V1_SX300.jpg', // Lagaan
-  'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVlLTM5YTgtZWNmNWMzMjZlMzFiXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg', // The Matrix
-  'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg', // Godfather
-  'https://m.media-amazon.com/images/M/MV5BNzA5ZDJhZWMtODU5NS00NDkyLWI4NTAtN2Y4NDI2NzI4MTEyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg', // Pulp Fiction
+  '/posters/xlaY2zyzMfkhk0HSC5VUwzoZPU1.webp',
+  '/posters/Interstellar_film_poster.jpg',
+  '/posters/5151N2hUPiL._AC_UF894,1000_QL80_.jpg',
+  '/posters/61NSZeiNF3L._AC_UF894,1000_QL80_.jpg',
+  '/posters/61NhAcNBC+L._AC_UF894,1000_QL80_.jpg',
+  '/posters/71CcdFCKHwL.jpg',
+  '/posters/Joker_-_Put_On_A_Happy_Face_-_Joaquin_Phoenix_-_Hollywood_English_Movie_Poster_3_0e557717-f9ae-4d45-82c3-27e08c2a9eeb.jpg',
+  '/posters/Dv4RO5OVYAUdsH0.jpg',
+  '/posters/345fecf5e269212d9a287508648ec173.jpg',
+  '/posters/OmShantiOm-ShahRukhKhanandDeepikaPadukone-BollywoodHindiMoviePoster_fcc8cd85-618b-4800-89f8-03bc7f507be7.jpg',
+  '/posters/images.jpg',
 ];
 
 const Hole = () => (
